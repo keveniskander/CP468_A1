60ae0289a1efc1bc171c9140c6d8091170f33dfb
Keven Iskander <keven.iskander@hotmail.com>
2020-10-06 21:27:38 -0400

    constructing puzzle class

--- 8puzzle.py
+++ 8puzzle.py
@@ -13,15 +13,30 @@ __updated__ = "2020-10-06"
 
 import utilities
 import math
-
-# This class represents the 8-puzzle
+import random
 
 # Table = [[][][],[][][],[][][]]
 
+# table = [[random.sample(fill,3)] for i in range(3)]
+
+
+
+# This class represents the 8-puzzle
 class Puzzle:
 
-    fill = '0123456789 '
+    def checkTable(self, table, size, key):
+        for i in range(size):
+            if key in table[i]:
+                return True
+        return False
+
+    fill = '12345678 '
     def __init__(self, size, table):
         self.size = size
         self.table = [[] for i in range(size)]
+        for j in range(3):
+            while len(table[j])<3:
+                temp = random.choice(self.fill)
+                if self.checkTable(table, 3, temp) == False:
+                    self.table[j].append(temp)
         
