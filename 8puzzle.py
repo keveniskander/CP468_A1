38b792c2ed562ea72eb9cbdd7d60903612e58b3e
Keven Iskander <keven.iskander@hotmail.com>
2020-10-06 20:30:24 -0400

    8puzzle added to repo

---
+++ 8puzzle.py
@@ -0,0 +1,13 @@
+"""
+-------------------------------------------------------
+8puzzle.py
+A* algorithm for the 8-puzzle, with heuristics h1 and h2, as
+seen in class.
+-------------------------------------------------------
+CP468
+Assignment 1
+Authors:  Keven Iskander, Carla Castaneda, Nicole Laslavic, Alex Francis
+__updated__ = "2020-10-06"
+-------------------------------------------------------
+"""
+
